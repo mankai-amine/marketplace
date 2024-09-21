7ce393abfe6d979573e31231d1fd7dfb17b2229d
mankai-amine <medaminemankai@gmail.com>
2024-09-21 19:30:48 -0400

    update checkout controller
    minor changes

--- src/main/java/org/styd/store/controller/CheckoutController.java
+++ src/main/java/org/styd/store/controller/CheckoutController.java
@@ -2,6 +2,7 @@ package org.styd.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.styd.store.entity.User;
@@ -10,14 +11,13 @@ import org.styd.store.exception.PaymentFailureException;
 import org.styd.store.securingweb.CustomUserDetails;
 import org.styd.store.service.CheckoutService;
 
-@RestController
-@RequestMapping("/buyer/checkout")
+@Controller
 public class CheckoutController {
 
     @Autowired
     private CheckoutService checkoutService;
 
-    @PostMapping
+    @PostMapping("/buyer/checkout")
     public String checkout(@AuthenticationPrincipal CustomUserDetails currentUser, RedirectAttributes redirectAttributes) {
 
         User buyer = currentUser.getUser();
@@ -28,10 +28,10 @@ public class CheckoutController {
             return "redirect:/";
         } catch (InsufficientStockException | PaymentFailureException e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage());
-            return "redirect:/checkout";
+            return "redirect:/buyer/cart";
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("error", "Checkout failed.");
-            return "redirect:/checkout";
+            return "redirect:/buyer/cart";
         }
     }
 
